edc1fd3d77c46d99474464936869ad183931fe1e
Hatem Soliman <hatemthedev@gmail.com>
2023-06-16 16:57:25 +0300

    Exercise 2 - First Attempt

---
+++ two.java
@@ -0,0 +1,17 @@
+// Exrecise 2
+public class two {
+  public static void main(String[] args) {
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+    String op = args[args.length - 1];
+    Arithmetic(x, y, op);
+  }
+
+  public static void Arithmetic(int x, int y, String op) {
+    if (op == "+") System.out.println(x +" "+ op +" "+ y +" = "+ (x + y));
+    else if (op == "-") System.out.println(x +" "+ op +" "+ y +" = "+ (x - y));
+    else if (op == "*") System.out.println(x +" "+ op +" "+ y +" = "+ (x * y));
+    else if (op == "/") System.out.println(x +" "+ op +" "+ y +" = "+ (x / y));
+    else System.out.println("Invalid Operation");;
+  }
+}
