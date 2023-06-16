961ea81888b1ff372c1f43e7b663d63c2d1199f9
Hatem Soliman <hatemthedev@gmail.com>
2023-06-16 17:53:52 +0300

    Exercise 4- Corrected

--- four.java
+++ four.java
@@ -2,7 +2,7 @@
 public class four {
   public static void main(String[] args) {
     int[] a = {1,2,3,4};
-    int[] b = {5,2};
+    int[] b = {5,0,2};
     int[] c = {6,6};
     System.out.println(minRec(a));
     System.out.println(minRec(b));
@@ -10,14 +10,16 @@ public class four {
   }
 
   public static int minRec(int[] a) {
-    return isHelper(a, 0, Integer.MAX_VALUE);
+    return isHelper(a, 0, a[0]);
   }
 
   public static int isHelper(int[] a, int index, int min) {
     if (index == a.length) return min;
     else {
-      if (a[index] < min) return isHelper(a, ++index, a[index]);
+      if (a[index] < min) {
+        min = a[index];
+      }
+      return isHelper(a, ++index, min);
     }
-    return min;
   }
 }
